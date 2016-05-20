06e90d7a25b07fa62940b896eed093db1d89ebab
Steven B. Oladipo <stevenboladipo@gmail.com>
2016-05-19 20:39:25 -0400

    updates to express

---
+++ controller/controller.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/map', function(req, res, next) {
+  res.render('map', { title: 'Map' });
+});
+
+
+
+module.exports = router;
