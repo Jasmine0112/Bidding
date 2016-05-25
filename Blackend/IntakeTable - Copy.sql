74c05acec4b860dcb23547a4373cf53a3c79513a
Jasmine0112 <Jasminemorrison12@yahoo.com>
2016-05-24 19:28:17 -0700

    updates

--- Blackend/IntakeTable - Copy.sql
+++ Blackend/IntakeTable - Copy.sql
@@ -1,16 +1,15 @@
-CREATE DATABASE  `bidding_project`;
 USE `bidding_project`;
 
-CREATE TABLE `User_regristration` (
+CREATE TABLE `manufacturer regristration` (
 	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
 	`email` VARCHAR( 255 ) NOT NULL,
-	`password` VARCHAR( 255 ) NOT NULL,
 	`NameFirst` VARCHAR( 255 ) NOT NULL,
 	`NameLast` VARCHAR( 255 ) NOT NULL,
 	`Phone` VARCHAR( 255 ) NOT NULL,
 	`Companyname` VARCHAR( 255 ) NOT NULL,
-	`Streetaddress` VARCHAR( 255 ) NOT NULL,
-	`State` VARCHAR( 255 ) NOT NULL,
-	`country` VARCHAR( 255 ) NOT NULL,
+	`quantity` VARCHAR( 255 ) NOT NULL,
+	`Category` VARCHAR( 255 ) NOT NULL,
 	`Industry` VARCHAR( 255 ) NOT NULL,
+	`Product image` VARCHAR( 255 ) NOT NULL,
+	`Comments` VARCHAR( 255 ) NOT NULL,
 	PRIMARY KEY ( `id` ) );
--- controller/controller.js
+++ controller/controller.js
@@ -1,16 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: "Ibrahim02",
+    database: "bidding_project"
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+<!--note to self put hbs file name int he section after render. hsb ending needed-->
 router.get('/map', function(req, res, next) {
   res.render('map', { title: 'Map' });
 });
 
+router.get('/buyer', function(req, res, next) {
+  res.render('UserlandingPage', { title: 'Buyer' });
+});
 
+router.get('/Manufacturer', function(req, res, next) {
+	connection.query('select * from table name  WHERE colume=?',[req.session.instudy], function (error, results, fields) {
+
+
+  res.render('ManfacturerLandingPage', { data: results });
+
+	})
+});
+
+router.get('/Regristration', function(req, res, next) {
+  res.render('Regristrationpage', { title: 'Regristration' });
+});
+
+router.post('/Regristration', function(req, res, next) {
+	connection.query('INSERT INTO users (Insert all form fields but a common each) values(req.body value name')
+ });
 
 module.exports = router;
